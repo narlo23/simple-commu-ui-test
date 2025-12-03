6c64a370a711329b7110aad14d0c31e6440aaaa6
narlo23 <aks152312@gmail.com>
2025-12-03 16:48:58 +0900

    [feature] 로그인 환영 메시지 검증 추가

--- tests/exam/test_login.py
+++ tests/exam/test_login.py
@@ -21,8 +21,9 @@ def login_success(driver):
     print("✅ URL 검증 성공: 메인 페이지로의 리다이렉션이 완료되었습니다.")
 
     login_username = get_element_by_testid(driver, "text-username").text
+    welcome_username = get_element_by_testid(driver, "text-welcome-username").text
     print(f"유저명: {login_username}, 회원가입한 유저명: {USERNAME}")
-    assert login_username == USERNAME, "❌ 오류: 유저명이 일치하지 않습니다.."
+    assert login_username == USERNAME and welcome_username == USERNAME, "❌ 오류: 유저명이 일치하지 않습니다.."
     print("✅ 유저명 검증 성공: 정상 로그인되었습니다.")
 
     check_cookie(driver, "connect.sid")
